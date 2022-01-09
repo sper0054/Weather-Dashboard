51faf49a75f76874630b52c2e5aab4f1a187a52c
sper0054 <teresa.sperl@gmail.com>
2022-01-09 14:36:38 -0600

    final touches

--- assets/script.js
+++ assets/script.js
@@ -1,28 +1,26 @@
 
-//display the top half of the webpage (today or current weather condition)
+//current weather
 var displayCurrentWeather = function(name, date, temp, humidity, wind_speed, uvi){
-    //arr to hold information to concat later
     var dataArr = [temp, humidity, wind_speed, uvi]
     var nameArr = ["Temp: ", "Wind: ", "Humidity: ", "UV Index: "]
     var unitArr = ["\u00B0F", " MPH", " %"]
 
-    //converting binary time into UTC time
     var dateObj = new Date(date * 1000).toLocaleString();
     var convertDate = dateObj.slice(0,8);
 
     var currentWeather = $("<div>").attr("id", "current").addClass("row current d-flex flex-column justify-content-around");
     var h2El = $("<h2>").attr("id", "city-name").addClass("pl-3 pt-2").text(name.toUpperCase() + " (" + convertDate + ")");
-    currentWeather.append(h2El) // append the city name and date.
-
-    //append the temp, wind humidity and uv index
+    currentWeather.append(h2El) 
+ 
     for(var i = 0; i < 4; i++){
         if (i < 3){
             var pEl = $("<p>").attr("id", dataArr[i]).text(nameArr[i] + dataArr[i] + unitArr[i]);
             currentWeather.append(pEl);
-        } else { // change the color of background of UV index depending on how high the UV index is serve as a warning. 
+        } else { 
             var spanEl = $("<span>").addClass("text-light pl-3 pr-3 text-center").text(dataArr[i]);
             pEl = $("<p>").attr("id", dataArr[i]).text(nameArr[i]);
 
+            //UV colors
             if (dataArr[i] <= 2){
                 spanEl.addClass("bg-success");
                 pEl.append(spanEl);
@@ -45,10 +43,10 @@ var displayCurrentWeather = function(name, date, temp, humidity, wind_speed, uvi
 
     $("#display-data").append(currentWeather);
 
-    create5DaySection(); //create the 5-day forecast section after the current section render
-
+    create5DaySection();
 };
 
+// 5 day forecast
 var create5DaySection = function(){
     var divEl = $("<div>").addClass("row-md d-flex flex-column justify-content-around forecast")
     var h2El = $("<h3>").text("5-Day Forecast:").addClass("pt-3 pb-2");
@@ -63,29 +61,22 @@ var create5DaySection = function(){
 var display5Day = function (date, temp, humidity, wind_speed, icon, description){
     var dateObj = new Date(date * 1000).toLocaleString();
     var convertDate = dateObj.slice(0,8);
-
     var card = $("<div>").addClass("card align-self-md-stretch days")
-    //title/date
     var cardBody = $("<div>").addClass("card-body pt-0 pl-0 pr-0 pb-0");
-
     var cardTitle = $("<p>").addClass("title").text(convertDate);
     cardBody.append(cardTitle);
 
-    //div - image
     var divEl = $("<div>");
     var cardImg = $("<img>").attr({src:"http://openweathermap.org/img/wn/" + icon +".png", alt: description});
     divEl.append(cardImg);
     cardBody.append(divEl);
 
-    //temp
     var tempEl = $("<p>").addClass("data").text("Temp: " + temp + "\u00B0F");
     cardBody.append(tempEl);
-
-    //Wind
+    
     var windEl = $("<p>").addClass("data").text("Wind: " + wind_speed + " MPH");
     cardBody.append(windEl);
 
-    //humidity
     var humidityEl = $("<p>").addClass("data").text("Humidity: " + humidity + " %")
     cardBody.append(humidityEl);
 
@@ -94,7 +85,7 @@ var display5Day = function (date, temp, humidity, wind_speed, icon, description)
     $("#5DayCard").append(card);
 };
 
-//get the weather base on lat and lot
+//weather details
 var getWeather = function(cityName,lon, lat){
     var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=9698d78e4b0b91d10c1cae15ee7197eb"
 
@@ -103,9 +94,9 @@ var getWeather = function(cityName,lon, lat){
             response.json().then(function(data){
                 console.log(data);
                 for(var i = 0; i < 6; i++){
-                    if (i === 0){ //first index is today weather
+                    if (i === 0){
                         displayCurrentWeather(cityName, data.daily[i].dt, data.daily[i].temp.day, data.daily[i].humidity, data.daily[i].wind_speed, data.daily[i].uvi);
-                    } else { //the rest is 5 day forecast, need the icon for the image.
+                    } else {
                         display5Day(data.daily[i].dt, data.daily[i].temp.day, data.daily[i].humidity, data.daily[i].wind_speed, data.daily[i].weather[0].icon, data.daily[i].weather[0].description);
                     }
                 }
@@ -115,7 +106,7 @@ var getWeather = function(cityName,lon, lat){
     })
 };
 
-//get the lat and lon depend on the city that passed in
+//city latlon
 var getLatLon = function(city){
     var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=9698d78e4b0b91d10c1cae15ee7197eb"
 
@@ -123,7 +114,7 @@ var getLatLon = function(city){
         if (response.ok){
             response.json().then(function(data){
                 getWeather(city, data.coord.lon, data.coord.lat);
-                saveData(city); //only save if the search is valid
+                saveData(city);
             })
         } else {
             console.log("error: please enter a valid city name");
@@ -131,13 +122,13 @@ var getLatLon = function(city){
     })
 };
 
-//create button for each local save city
+//city buttons
 var createBtn = function(name){
     var btnEl = $("<button>").addClass("btn btn-secondary mb-2").attr("id", "submit").text(name);
     $("#history").append(btnEl);
 };
 
-//save the search city into the local save
+//save city to local storage
 var saveData = function(name){
     var citySearch = JSON.parse(localStorage.getItem("cityName")) || []
     var nameUpper = name.toUpperCase();
@@ -145,7 +136,7 @@ var saveData = function(name){
     if(citySearch.length === 0){
         citySearch.push(nameUpper);
         createBtn(nameUpper);
-    } else if (!citySearch.includes(nameUpper)){ // only push the search into local storage if the city isnt in local storage
+    } else if (!citySearch.includes(nameUpper)){
         citySearch.push(nameUpper);
         createBtn(nameUpper);
     }   
@@ -162,7 +153,7 @@ var loadData = function(){
     })
 }
 
-//run the script when the search button is click
+//search button click
 $("#search").on("click", function(event){
     event.preventDefault();
     document.getElementById("display-data").innerHTML = ""
@@ -173,7 +164,7 @@ $("#search").on("click", function(event){
 
 });
 
-//listen to dynamic created button from local storage and passed the value into the program
+//city button click
 $("#history").on("click", "#submit", function(){
     var city = $(this).text();
     document.getElementById("display-data").innerHTML = "";
